8ccefcb212d58868bdd0af0e41ba143afcd63abb
alex <alex.hmn18@gmail.com>
2025-02-21 11:36:42 +0100

    build: ajout des fixtures user avec mot de passe hashé

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,18 +2,55 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
 
-        // $product = new Product();
-        // $manager->persist($product);
+        // ---------------
+        // Ajout des Users
+        // ---------------
+        $admin_user = new User();
+        $admin_user->setEmail('admin@test.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->hasher->hashPassword($admin_user, 'admin123'))
+            ->setNom('Test')
+            ->setPrenom('Admin')
+            ->setDateDeNaissance(new DateTimeImmutable())
+            ->setMisAJourLe(new DateTimeImmutable());
+        $manager->persist($admin_user);
+
+        $eleveur_user = new User();
+        $eleveur_user->setEmail('eleveur@test.com')
+            ->setRoles(['ROLE_ELEVEUR'])
+            ->setPassword($this->hasher->hashPassword($eleveur_user, 'eleveur123'))
+            ->setNom('Test')
+            ->setPrenom('Eleveur')
+            ->setDateDeNaissance(new DateTimeImmutable())
+            ->setMisAJourLe(new DateTimeImmutable());
+        $manager->persist($eleveur_user);
+
+        $particulier_user = new User();
+        $particulier_user->setEmail('particulier@test.com')
+            ->setRoles(['ROLE_PARTICULIER'])
+            ->setPassword($this->hasher->hashPassword($particulier_user, 'part123'))
+            ->setNom('Test')
+            ->setPrenom('Particulier')
+            ->setDateDeNaissance(new DateTimeImmutable())
+            ->setMisAJourLe(new DateTimeImmutable());
+        $manager->persist($particulier_user);
+
 
         $manager->flush();
     }
