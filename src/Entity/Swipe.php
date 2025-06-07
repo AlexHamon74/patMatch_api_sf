4a742fce03ada09e91a9b7d48379e72bc1577129
alex <alex.hmn18@gmail.com>
2025-06-07 12:59:20 +0200

    feat: modif groupe de serialisation

--- src/Entity/Swipe.php
+++ src/Entity/Swipe.php
@@ -38,7 +38,7 @@ class Swipe
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
-    #[Groups(['swipe:write', 'eleveur:read'])]
+    #[Groups(['swipe:write', 'eleveur:read', 'client:read'])]
     private ?string $type = null;
 
     #[ORM\ManyToOne(inversedBy: 'swipes')]
