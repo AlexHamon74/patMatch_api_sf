fcd93b0fa50744a4d88b45854ceb9068b898d116
alex <alex.hmn18@gmail.com>
2025-06-08 19:38:09 +0200

    feat: mise en place upload image profile users

--- src/Controller/UpdateUserImageController.php
+++ src/Controller/UpdateUserImageController.php
@@ -7,14 +7,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UpdateUserImageController extends AbstractController
 {
-    public function __invoke(int $id, Request $request, EntityManagerInterface $em, UserInterface $user): JsonResponse
+    public function __invoke(int $id, Request $request, EntityManagerInterface $em, ?UserInterface $user = null): JsonResponse
     {
         $userToUpdate = $em->getRepository(User::class)->find($id);
 
@@ -22,8 +23,8 @@ class UpdateUserImageController extends AbstractController
             throw new NotFoundHttpException("Utilisateur non trouvé.");
         }
 
-        // Vérifier si l’utilisateur connecté est bien celui ciblé ou admin
-        if ($user !== $userToUpdate && !$this->isGranted('ROLE_ADMIN')) {
+        // Facultatif : restreindre les droits si un utilisateur est connecté
+        if ($user && $user !== $userToUpdate && !$this->isGranted('ROLE_ADMIN')) {
             throw new AccessDeniedHttpException("Vous n'avez pas le droit de modifier cet utilisateur.");
         }
 
